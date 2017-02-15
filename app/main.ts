c14570cca4c4cec974c61aa45b99c21eaac8b45f
Manjunath Moorthi <manjunathmoorthi@gmail.com>
2017-02-14 20:30:28 -0600

    Update main.ts

--- app/main.ts
+++ app/main.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { RacesComponent } from './races.component';
+import { FormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [ BrowserModule ],
+  imports: [ BrowserModule, FormsModule ],
   declarations: [ AppComponent , RacesComponent ],
   bootstrap: [ AppComponent ]
 })
