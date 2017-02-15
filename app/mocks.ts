3a1d1d67a4e72548b33297e58e3ce49528ef838f
Manjunath Moorthi <manjunathmoorthi@gmail.com>
2017-02-14 20:31:41 -0600

    Update mocks.ts

--- app/mocks.ts
+++ app/mocks.ts
@@ -6,19 +6,25 @@ import { race } from './race';
   "date": new Date('2512-01-04T14:00:00'),
   "about": "Race through the ruins of an ancient Florida battle arena.",
   "entryFee": 3200,
-  "isRacing": false
+  "isRacing": false,
+  "image": "http://www.thunderdomeracing.com/images/logo.png",
+  "imageDescription": "Race track with laser lanes"
 }, {
   "id": 2,
   "name": "San Francisco Ruins",
   "date": new Date('2512-07-03T20:00:00'),
   "about": "Drift down the streets of a city almost sunk under the ocean.",
   "entryFee": 4700,
-  "isRacing": true
+  "isRacing": true,
+  "image": "https://s-media-cache-ak0.pinimg.com/originals/66/10/91/6610915c5ec7a7458761089cd9c15e67.jpg",
+  "imageDescription": "Golden Gate Bridge with lasers"
 }, {
   "id": 3,
   "name": "New York City Skyline",
   "date": new Date('2512-07-12T21:00:00'),
   "about": "Fly between buildings in the electronic sky.",
   "entryFee": 4300,
-  "isRacing": true
+  "isRacing": true,
+  "image": "http://www.nationalgeographic.com/new-york-city-skyline-tallest-midtown-manhattan/assets/img/articleImg/01nyskyline640.jpg",
+  "imageDescription": "Bridge overlooking New York City"
 }];
