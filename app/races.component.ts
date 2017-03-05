442d790c3f119e02198eb04dac33a9a7b95a6246
Manjunath Moorthi <manjunathmoorthi@gmail.com>
2017-03-05 15:06:12 -0600

    Update races.component.ts

--- app/races.component.ts
+++ app/races.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import { race } from './race';
-import { RACES } from './mocks';
+import { Race } from './race';
+import { RaceService } from './race.service';
 
 @Component({
   selector: 'my-races',
@@ -10,29 +10,43 @@ import { RACES } from './mocks';
 export class RacesComponent {
   heading = "Ultra Racing Schedule"
   cash = 10000;
-  races: race[];
+  races: Race[];
+
+  constructor(private raceService: RaceService) { }
 
   ngOnInit() {
-    this.races = RACES;
+    this.raceService.getRaces()
+        .subscribe(data => this.races = data);
   }
 
   totalCost() {
     let sum = 0;
-    for (let race of this.races) {
-      if (race.isRacing) sum += race.entryFee;
+    if (this.races) {
+      for (let race of this.races) {
+        if (race.isRacing) sum += race.entryFee;
+      }
     }
     return sum;
+  }
+
+  castDate(date) {
+    return new Date(date);
   }
 
   cashLeft() {
     return this.cash - this.totalCost();
   }
+
+  enterRace(race) {
+    if (this.cashLeft() > race.entryFee) {
+      race.isRacing = true;
+    } else {
+      alert("You don't have enough cash");
+    }
+  }
+
   cancelRace(race) {
-  race.isRacing = false;
-}
- enterRace(race){
-   if (this.cashLeft()>race.entryFee)
-     race.isRacing=true;
-   else alert("You don't have enough cash");
- }
+    race.isRacing = false;
+  }
+
 }
